63b742e4fff01a1cd782227d4d2bf2246f8c2488
FabianUlises <ulisesfab.car@outlook.com>
2023-03-02 13:59:40 -0800

    fixing heroku error

--- server.js
+++ server.js
@@ -3,15 +3,25 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const app = require('./app');
 // Mongoose
-const connect = async () => {
-    try {
-        // Connecting to mongoose
-        mongoose.connect(process.env.MONGO_URI);
-    } catch(err) {
-        console.log(err, 'mongoose connection error');
-    }
-}
-connect();
+// const connect = async () => {
+//     try {
+//         // Connecting to mongoose
+//         mongoose.connect(process.env.MONGO_URI, {
+//             useNewUrlParser: true,
+//             UseUnifiedTopology: true
+//         });
+//         // Test console log
+//         console.log(`mongoose connected to db: ${process.env.MONGO_URI}`);
+//     } catch(err) {
+//         // Test console log
+//         console.log(err, 'Mongoose connection error');
+//     }
+// }
+// connect();
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 // Server on
 app.listen(process.env.PORT, () => {
     console.log("Server is running.");
