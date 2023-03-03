9377ded33fdd9d7620b41d6ede7671be3b8bdfec
FabianUlises <ulisesfab.car@outlook.com>
2023-03-03 10:59:19 -0800

    Added compression npm package

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 // Dependencies
 const express = require('express');
 const cors = require('cors');
+const compression = require('compression');
 const app = express();
 const booksController = require('./controllers/booksController');
 // Middleware
 app.use(cors());
+app.use(compression());
 app.use(express.json());
 // Routes
 app.get('/', (req, res) => {
--- server.js
+++ server.js
@@ -3,6 +3,20 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const app = require('./app');
 // Mongoose
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+    })
+    .then((conn) => {
+        console.log(`connected to db: ${process.env.MONGO_URI}`);
+    })
+    .catch((err) => {
+        console.log('Unable to connect to mongoose', err);
+    })
+// Server on
+app.listen(process.env.PORT, () => {
+    console.log("Server is running.");
+});
 // const connect = async () => {
 //     try {
 //         // Connecting to mongoose
@@ -18,11 +32,3 @@ const app = require('./app');
 //     }
 // }
 // connect();
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
-// Server on
-app.listen(process.env.PORT, () => {
-    console.log("Server is running.");
-});
